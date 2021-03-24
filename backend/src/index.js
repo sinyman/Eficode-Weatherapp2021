7d3a1be83e7582f2aaa46a9bf5e9e14f67912a74
Simon Nyman <nyman.simon@hotmail.com>
2021-03-24 16:46:39 +0200

    Change backend way of supplying API-key

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,6 @@ const router = require('koa-router',)();
 const fetch = require('node-fetch',);
 const cors = require('kcors',);
 
-require('dotenv',).config();
-
 const appId = process.env.APPID || '';
 const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
 const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
